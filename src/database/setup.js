2ab9154332c489b8e137129ce819021212b28174
rickstylz01 <rickmaya@gmail.com>
2021-05-14 21:21:24 -0500

    setup database

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,3 +1,17 @@
 // Setup mongoose
 const mongoose = require('mongoose');
 const connectionString = 'mongodb://localhost:27017/event-manager';
+
+module.exports = function() {
+	mongoose.connect(connectionString, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	}, (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('database connection successful');
+		}
+	})
+}
