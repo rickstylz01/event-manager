8a635cdc40395eb02eb04eb0d0a7ea0c13af4220
rickstylz01 <rickmaya@gmail.com>
2021-05-16 16:08:12 -0500

    add db.js file with connection to mongodb atlas

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://rmaya13:manimals13@newcluster.hojt2.mongodb.net/event-manager?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
